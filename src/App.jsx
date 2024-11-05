cf67b457fb67b9b2f812bbaeec54f5ae0fec16e2
Anas Bashir <nasbas23@gmail.com>
2024-11-06 02:43:07 +0300

    Setup commit

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Header from "./components/Header";
+import About from "./components/About";
+import Skills from "./components/Skills";
+import Projects from "./components/Projects";
+import Education from "./components/Education";
+import Experience from "./components/Experience";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+
+function App() {
+    return (
+        <div id="app">
+            <Header />
+            <About />
+            <Skills />
+            <Projects />
+            <Education />
+            <Experience />
+            <Contact />
+            <Footer />
+        </div>
+    );
+}
+
+export default App;
