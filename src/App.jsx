36d4120015c9e3931bd5046cdc7f3e6f48964d88
Anas Bashir <nasbas23@gmail.com>
2024-11-07 22:14:33 +0300

    Add Experience

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import About from "./components/About";
 import Education from "./components/Education";
+import Experience from "./components/Experience";
 import Header from "./components/Header";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
@@ -12,6 +13,7 @@ function App() {
             <Skills />
             <Projects />
             <Education />
+            <Experience />
         </div>
     );
 }
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,5 +1,40 @@
+const ExperienceItem = ({ role, company, period, description }) => (
+    <div className="mb-8">
+        <h3 className="text-xl font-semibold">{role}</h3>
+        <p className="dt dark:text-gray-400 mb-2">
+            {company} | {period}
+        </p>
+        <ul className="list-disc list-inside">
+            {description.map((item, index) => (
+                <li key={index} className="mb-1">
+                    {item}
+                </li>
+            ))}
+        </ul>
+    </div>
+);
+
 function Experience() {
-	return <h1>Experience</h1>;
+    const experiences = [
+        {
+            role: "Web Design Internship",
+            company: "Acugence Doha - Qatar",
+            period: "July 2024 - Sept 2024",
+            description: [
+                "Assisted in developing a waste management software solution by creating wireframes and user flows using tools like Figma and Flowmapp.",
+                "Utilized AI tools (like ChatGPT) to enhance design processes and collaborated with interdisciplinary teams to align project goals and requirements.",
+            ],
+        },
+    ];
+
+    return (
+        <section id="experience" className="lg:w-2/3">
+            <h2 className="text-3xl font-bold mb-8">Experience</h2>
+            {experiences.map((exp, index) => (
+                <ExperienceItem key={index} {...exp} />
+            ))}
+        </section>
+    );
 }
 
-export default Experience
+export default Experience;
